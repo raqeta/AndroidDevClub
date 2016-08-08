21e089992d1eb4e2d1bf4148e4c8f1617c2486f2
Aleksandr Ershov <4metall@gmail.com>
2016-08-08 21:51:26 +0300

    init project

---
+++ app/src/main/java/com/pitzunda/androiddevclub/MainActivity.java
@@ -0,0 +1,15 @@
+package com.pitzunda.androiddevclub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // java code
+    }
+}
